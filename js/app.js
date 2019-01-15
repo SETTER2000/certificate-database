f63f1957191c262263b5aa14e827652c378077cc
SETTER <info@lphp.ru>
2019-01-15 18:31:14 +0300

    Adden new func

--- js/app.js
+++ js/app.js
@@ -1,62 +1,79 @@
-/*const crt = (name, code) => ({name, code});
- const log = (text, type, date=new Date())=>({text, type, date});
- const crts = [
- crt("2-Port ACS800 Serial ord:  Plug CEE 7/7 to connector C13", "ACS8008SAC-404"),
- crt("2-Port ACS800 Serial ord:  Plug CEE 7/7 to connector C13", "ACS8008DAC-404"),
- crt("2-Port ACS800 Serial Console with analog modem",  "ACS8008MDAC-404"),
- crt("2-Port ACS800 Serial Console with analog modem", "ACS8016SAC-404"),
- crt("4-Port ACS800 Serial  Plug CEE 7/7 to connector C13", "ACS8016DAC-404"),
- crt("4-Port ACS800 Serial  Plug CEE 7/7 to connector C13", "ACS8016MDAC-404"),
- crt("4-Port ACS800 Serial Console with analog modem", "ACS8032SAC-404"),
- crt("4-Port ACS800 Serial Console with analog modem", "ACS8032DAC-404")
- ];*/
-
 new Vue({
     el: '#app',
     data() {
         return {
-            crts: null,
-             crt: crts[0],
             logs: [],
+            modalVisibility: false,
+            crts: null,
+            crt: null,
+            search: null,
             selectedCrtIndex: 0,
-            // phoneVisibility: false,
-            search: '',
-            modalVisibility: false
+            errored: false,
+            loading: true
         };
     },
     mounted() {
-        axios.get('http://www.landata.ru/api.php')
-            .then(response => (this.crts = response.data));
+        axios
+            .get('/api/v1/crts/')
+            .then(response => {
+                console.log(response);
+                this.crts = response.data;
+            })
+            .catch(error => {
+                console.log(error.response);
+                this.errored = true;
+            })
+            .finally(() => {
+                //console.log('this.errore22d', this.errored);
+                this.loading = false;
+            });
     },
+
     methods: {
         selectCrt(i) {
             //console.log('Click', i);
-            this.crt = crts[i];
+            this.crt = this.crts[i];
             this.selectedCrtIndex = i;
         },
-        downloadZIP(){
-            this.modalVisibility = false;
-            this.logs.push(log(`Success download: ${this.crt.code}`, 'ok'))
-        },
-        downloadCancel(){
-            this.modalVisibility = false;
-            this.logs.push(log(`Cancelled download: ${this.crt.code}`, 'cnl'))
-        },
-        filteredCrts() {
-            console.log('this.crts:', this.crts);
-            return this.crts.filter(crt => crt.name.indexOf(this.search) > -1 || crt.code.indexOf(this.search) > -1);
+        /*     downloadZIP(){
+         this.modalVisibility = false;
+         this.logs.push(log(`Success download: ${this.crt.code}`, 'ok'))
+         },
+         downloadCancel(){
+         this.modalVisibility = false;
+         this.logs.push(log(`Cancelled download: ${this.crt.code}`, 'cnl'))
+         },
+         filteredCrts() {
+         console.log('this.crts:', this.crts);
+         return this.crts.filter(crt => crt.name.indexOf(this.search) > -1 || crt.code.indexOf(this.search) > -1);
 
+         }*/
+        customFilter (val) {
+            // ...
+            return something
         }
     },
     computed: {
-        /* phoneBtnText() {
-         return this.phoneVisibility ? 'Скрыть телефон' : 'Показать телефон'
-         },*/
-
+        filterCrtsRegisterNot() {
+            return !this.search ? ''
+                :
+                this.crts.filter(
+                    crt => crt.name.toLowerCase().indexOf(this.search) > -1 ||
+                    crt.code.toLowerCase().indexOf(this.search) > -1 ||
+                    crt.name.toUpperCase().indexOf(this.search) > -1 ||
+                    crt.code.toUpperCase().indexOf(this.search) > -1
+                );
+        },
+        filterCrtsRegister() {
+            return !this.search ? '' : this.crts.filter(crt => crt.name.indexOf(this.search) > -1 || crt.code.indexOf(this.search) > -1);
+        }
     },
     filters: {
         date(val){
             return (val !== undefined) ? val.toLocaleString() : '';
         }
+       /* quotDel(val){
+            return (val !== undefined) ? val.replace( //gi, "*" ) : '';
+        }*/
     }
 });
